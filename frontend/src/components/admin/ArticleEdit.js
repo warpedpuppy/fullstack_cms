175ccdc631830bf519626b47ec745285d70c9ccf
ted <ted@warpedpuppy.com>
2020-02-03 08:46:27 -0500

    edit article

--- frontend/src/components/admin/ArticleEdit.js
+++ frontend/src/components/admin/ArticleEdit.js
@@ -9,16 +9,20 @@ export default class ArticleEdit extends Component {
     }
     getArticleTitles = async () => {
         let res = await ArticleService.getArticleTitles();
-        
         if (res.success) {
             this.setState({titles: res.result})
         }
     }
     getArticle = async (id) => {
-        console.log(ArticleService)
         let res = await ArticleService.getArticleForEdit(id) 
-        console.log(res.result[0]);
         this.setState({editArticle: res.result[0]})
+    }
+    onChangeHandler = (e) => {
+        console.log(e.target.name, e.target.value)
+        let newObj = {};
+        newObj[e.target.name] = e.target.value;
+        let editArticle = Object.assign({}, this.state.editArticle, newObj)
+        this.setState({editArticle})
     }
     render() {
         let titles = this.state.titles.map( (title, i) => {
@@ -27,10 +31,23 @@ export default class ArticleEdit extends Component {
         if (!Object.keys(this.state.editArticle).length) {
             return ( <ul>{titles}</ul> )
         } else {
+            let {title, description,  content} = this.state.editArticle;
             return (
                 <div className='edit-article-form-cont'>
                 <form>
                     <h4>edit article</h4>
+                    <div>
+                        <label htmlFor="title">title: </label>
+                        <input onChange={this.onChangeHandler} type="text" name="title" id="title" value={title} />
+                    </div>
+                    <div>
+                        <label htmlFor="description">description: </label>
+                        <input onChange={this.onChangeHandler} type="text" name="description" id="description" value={description} />
+                    </div>
+                    <div>
+                        <label htmlFor="content">content: </label>
+                        <textarea onChange={this.onChangeHandler} name="content" value={content} />
+                    </div>
                 </form>
                 </div>
              
