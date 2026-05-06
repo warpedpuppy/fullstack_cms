c33f8c2046feaec9ef0237201b87a50799b448c1
ted <ted@warpedpuppy.com>
2026-05-06 13:07:24 -0400

    fix errors and add styling

--- frontend/src/components/events/EventModule.js
+++ frontend/src/components/events/EventModule.js
@@ -8,15 +8,19 @@ export default function EventModule(props) {
         history.push(`/event/${props.id}`)
     }
     return (
-        <div className="card event-module" style={{'width': '10rem'}} onClick={goToEvent}>
-            <img src={`/bmps/${props.img_url}`} class="card-img-top" alt="..."></img>
+        <div className="card event-module" onClick={goToEvent}>
+            <div className="card-img-wrapper">
+                <img 
+                    src={props.img_url ? `/bmps/${props.img_url}` : '/bmps/qr/default_image.jpeg'} 
+                    onError={(e) => { e.target.onerror = null; e.target.src = '/bmps/qr/default_image.jpeg' }} 
+                    className="card-img-top" 
+                    alt={props.eventname || 'Event'} 
+                />
+            </div>
             <div className="card-body">
-                
-            
                 <h5 className="card-title">{ props.eventname}</h5>
-                <p className="card-text">{ Utils.formatDate(props.date_of_event) }</p>
-                
-                <p className="card-text">{props.time_start} to {props.time_end}</p>
+                <p className="card-date">{ Utils.formatDate(props.date_of_event) }</p>
+                <p className="card-time">{props.time_start} to {props.time_end}</p>
             </div>
         </div>
     )
